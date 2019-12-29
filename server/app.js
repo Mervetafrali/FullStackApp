11cc38c07d128d04eac1fd9148bed3035207a9f1
mervetafrali <160202100@kocaeli.edu.tr>
2019-12-29 11:50:33 +0300

    mongodb connection

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const http = require("http").createServer(app);
+const userCreateRouter = require('./routes/createEmployee') ;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', userCreateRouter);
+
+
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb://localhost:27017/employess',{
+    userNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+},(err)=>{
+    if(err){
+        console.log('DB bağlantı hatası ${err}');
+        return;
+    }
+    console.log("MongoDB'ye bağlandık");
+});
+
+const port = 3000;
+http.listen(port, () =>
+  console.log(`Server localhost:${port} üzerinde ayakta`)
+);
